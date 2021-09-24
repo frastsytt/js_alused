76d0f50f86be48bed9c8f21854122c5ff36118ab
frastsytt <frastsytt@gmail.com>
2021-09-24 11:36:10 +0300

    Data types

---
+++ practice.js
@@ -0,0 +1,46 @@
+//Data types
+//Primitives
+//Text
+const name2 = 'Kadi Tamm';
+console.log(name2);
+console.log(typeof name2);
+
+const age = 15;
+console.log(age);
+console.log(typeof age);
+
+
+const height = 1.5;
+console.log(height);
+console.log(typeof height);
+
+const hasKids = false;
+console.log(hasKids);
+console.log(typeof hasKids);
+
+const hasAirplane = null;
+console.log(hasAirplane);
+console.log(typeof hasAirplane);
+
+let test;
+console.log(test);
+console.log(typeof test);
+
+//Reference types
+
+const hobbies = ['music', 'sport'];
+console.log(hobbies);
+console.log(typeof hobbies);
+
+const person2 = {
+	imya: 'Heino',
+	agee: 20
+}
+
+console.log(person2);
+console.log(typeof person2);
+
+
+const today = new Date();
+console.log(today);
+console.log(typeof today);
