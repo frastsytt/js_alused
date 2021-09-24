995141c2ba0c7528b93816d2949fb7f83c0ec9f9
frastsytt <frastsytt@gmail.com>
2021-09-24 13:00:01 +0300

    Math

---
+++ math.js
@@ -0,0 +1,25 @@
+let val;
+const num1 = 100;
+const num2 = 50;
+
+// Matemaatilised tehted
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math modules
+val = Math.PI;
+val = Math.round(Math.PI);
+val = Math.ceil(Math.PI);
+val = Math.floor(Math.PI);
+val = Math.sqrt(64);
+val = Math.abs(-(Math.PI));
+val = Math.pow((Math.PI), 2);
+val = Math.round(Math.random() * 10);
+val = Math.min(90, 190, 5, 900, 1000);
+val = Math.max(90, 190, 5, 900, 1000);
+
+console.log(val);
+console.log(typeof val);
